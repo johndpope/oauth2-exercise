f51d5262fab7f74ae0b847edce92087d98467ccc
Chris <user@macintosh-2.local>
2015-11-06 16:37:49 -0500

    Initial Commit

---
+++ InstagramPlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstagramPlus
+//
+//  Created by Chris on 11/6/15.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstagramPlus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstagramPlus
+//
+//  Created by Chris on 11/6/15.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
